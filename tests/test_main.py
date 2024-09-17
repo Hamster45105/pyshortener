241ce4411ef0767ac0760cd18493f6a3f3f2fc21
Hamish <133548095+Hamster45105@users.noreply.github.com>
2024-09-17 12:32:04 +1000

    Improve tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -29,13 +29,13 @@ class TestShorten(unittest.TestCase):
 
     def test_get_stats_valid_url(self):
         short_url = "UsQlQM"
-        stats = get_stats(short_url, 'hitsweek')
+        stats = get_stats(short_url, 'hitsweek', format_response=True)
         self.assertIsInstance(stats, str)
         self.assertIn("Date,Visits", stats)
 
     def test_get_stats_valid_url_with_title(self):
         short_url = "UsQlQM"
-        stats = get_stats(short_url, 'hitsweek', include_title=True)
+        stats = get_stats(short_url, 'hitsweek', include_title=True, format_response=True)
         self.assertIsInstance(stats, str)
         self.assertIn("Visits this week", stats)
         self.assertIn("Date,Visits", stats)
@@ -43,17 +43,17 @@ class TestShorten(unittest.TestCase):
     def test_get_stats_invalid_url(self):
         short_url = "invalid_url"
         with self.assertRaises(GenericError):
-            get_stats(short_url, 'hitsweek')
+            get_stats(short_url, 'hitsweek', format_response=True)
 
     def test_get_stats_invalid_stats_type(self):
         short_url = "UsQlQM"
         with self.assertRaises(ValueError):
-            get_stats(short_url, 'invalid_stats_type')
+            get_stats(short_url, 'invalid_stats_type', format_response=True)
 
     def test_get_stats_invalid_service(self):
         short_url = "UsQlQM"
         with self.assertRaises(ValueError):
-            get_stats(short_url, 'hitsweek', service="invalid_service")
+            get_stats(short_url, 'hitsweek', service="invalid_service", format_response=True)
 
 if __name__ == "__main__":
     unittest.main()
